ebf864a92290af7b7b6134c78fdc7ae91b0ef917
Philip Boden <philip.boden@gmail.com>
2026-03-12 03:05:54 -0700

    (fix) Config tweak and url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://paboden.github.io/markgodi.com',
+  site: 'https://paboden.github.io/markgodi_com',
   base: '/',
+  redirects: {
+    '/about': '/#about',
+    '/book': '/#book',
+    '/workshops': '/#workshops',
+    '/connect': '/#connect',
+    '/blog': '/#blog',
+  }
 });
